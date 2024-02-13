56fec45c83777002b8e3849c8ac3d27e29dfd8b3
michele-pupo <mikypupo@hotmail.it>
2024-02-13 14:10:07 +0100

    created array with provided objects

--- js/script.js
+++ js/script.js
@@ -12,3 +12,37 @@
 // - Ragioniamo come sempre a step.
 // - Prima la logica in italiano e poi traduciamo in codice.
 // - E ricordiamoci che console.log() è nostro amico!
+
+// creiamo un array di oggetti con le informazioni fornite
+const membersTeam = [
+    {
+    "name" : 'Wayne Barnett',
+    "role" : 'Founder & Ceo',
+    "photo": 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+    "name" : 'Angela Caroll',
+    "role" : 'Chief Editor',
+    "photo": 'angela-caroll-chief-editor.jpg'
+    },
+    {
+    "name" : 'Walter Gordon',
+    "role" : 'Office Manager',
+    "photo": 'walter-gordon-office-manager.jpg'
+    },
+    {
+    "name" : 'Angela Lopez',
+    "role" : 'Social Media Manager',
+    "photo": 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+    "name" : 'Scott Estrada',
+    "role" : 'Developer',
+    "photo": 'scott-estrada-developer.jpg'
+    },
+    {
+    "name" : 'Barbara Ramos',
+    "role" : 'Graphic Designer',
+    "photo": 'barbara-ramos-graphic-designer.jpg'
+    },
+];
